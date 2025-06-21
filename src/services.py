8df329951e4334356892a3d2f864672085c01f2f
Konstantin <kgranevitch@gmail.com>
2025-06-22 00:10:06 +0300

    add func tel_finder

--- src/services.py
+++ src/services.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def finder(list_transactions: list[dict], attribute: str) -> str:
@@ -12,5 +13,21 @@ def finder(list_transactions: list[dict], attribute: str) -> str:
 
     if result == []:
         return "искомые транзакции отсутствуют"
+    else:
+        return json.dumps(result, ensure_ascii=False)
+
+
+def tel_finder(list_transactions: list[dict]) -> str:
+    """Функция поиска транзакций в описании которых присутствуют телефонные номера"""
+
+    tel_number = re.compile(r'[+]\d\s\d{3}.\d{2}.\d{2}')
+    result = []
+
+    for transaction in list_transactions:
+        if re.search(tel_number, str(transaction)):
+            result.append(transaction)
+
+    if result == []:
+        return "транзакции с телефонными номерами отсутствуют"
     else:
         return json.dumps(result, ensure_ascii=False)
