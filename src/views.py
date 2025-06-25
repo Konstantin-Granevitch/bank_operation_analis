0f220453db4a4d21d045d0943be42b112c46b2f3
Konstantin <kgranevitch@gmail.com>
2025-06-25 22:53:40 +0300

    add functional API

--- src/views.py
+++ src/views.py
@@ -8,14 +8,15 @@ import requests
 from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
 
+from config import BASE_PATH
 from src.utils import xlsx_reader
 
 
-def api_currency(user_settings: str):
-    """функция возвращающая запрос API согласно файла пользовательской конфигурации"""
+def api_currency(user_settings: str = "user_settings.json") -> str:
+    """функция возвращающая запрос API с курсом валют согласно файла пользовательской конфигурации"""
 
     # получение пользовательских данных
-    with open("../data/" + user_settings) as f:
+    with open(f"{BASE_PATH}/data/{user_settings}") as f:
         file = json.load(f)
 
     currency = file["user_currencies"]
@@ -25,7 +26,7 @@ def api_currency(user_settings: str):
 
     # загрузка "dotenv" библиотеки для создания заголовка с API ключем
     load_dotenv()
-    api_key = os.getenv("API_KEY")
+    api_key = os.getenv("API_KEY_CURRENCY")
     headers = {"apikey": api_key}
 
     # получение курса валют согласно пользовательских данных
@@ -34,15 +35,26 @@ def api_currency(user_settings: str):
     return curse.json()
 
 
-def api_stoks(user_settings: str):
-    """"""
+def api_stoks(user_settings: str = "user_settings.json") -> str:
+    """функция возвращающая запрос API с курсом акций согласно файла пользовательской конфигурации"""
 
     # получение пользовательских данных
-    with open("../data/" + user_settings) as f:
+    with open(f"{BASE_PATH}/data/{user_settings}") as f:
         file = json.load(f)
 
     stoks = file["user_stocks"]
     symbols = ",".join(stoks)
+    url = f"https://financialmodelingprep.com/api/v3/quote/{symbols}?"
+
+    # загрузка "dotenv" библиотеки для создания заголовка с API ключем
+    load_dotenv()
+    api_key = os.getenv("API_KEY_STOKS")
+    headers = {"apikey": api_key}
+
+    # получение курса акций согласно пользовательских данных
+    stoks = requests.get(url, headers)
+
+    return stoks.json()
 
 
 def main_views(user_date: str, interval: str = "M") -> str:
@@ -77,4 +89,4 @@ if __name__ == "__main__":
     # list_transactions = xlsx_reader("operations.xlsx").to_dict('records')
     # pprint(main_views("01.01.2018"))
     # print(api_currency("user_settings.json"))
-    print(api_stoks("user_settings.json"))
+    pprint(api_stoks("user_settings.json"))
